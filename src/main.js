8440390956a9fb182868dca9b94a6a82b7b348af
Harry Moreno <morenoh149@gmail.com>
2014-05-27 19:37:36 -0700

    refactored code to use getDest. Animation direction logic can be tweaked here

--- src/main.js
+++ src/main.js
@@ -13,18 +13,10 @@ define(function(require, exports, module) {
     // create the main context
     var mainContext = Engine.createContext();
     var width = screen.width;
-    var random = new Random;
+    var height = screen.height;
+    var random = new Random();
     random.seed(Date.now());
 
-    function centerBias(num) {
-      if (num < 0.5)
-        return num+(0.2*Math.random());
-      else
-        return num-(0.2*Math.random());
-    }
-    function randBinary() {
-      return Math.random() < 0.5 ? 1 : -1;
-    }
     var bg = new Surface({
       size: [undefined, undefined],
       content: '<h1>Starfield</h1><h2>by <a href="http://morenoh149.github.io/">@morenoh149</a></h2>Made with <a href="http://famo.us">famo.us</a>. <a class="tweet" href="https://twitter.com/share?via=morenoh149&text=Check out this cool starfield @befamous" class="twitter-share-button" data-lang="en"><img src="img/twitter2.png" /></a><a href="https://github.com/morenoh149/starfield-famous"><img src="img/github.png" /></a>',
@@ -45,28 +37,35 @@ define(function(require, exports, module) {
             borderRadius: '15px'
           }
         });
-        //var xcen = centerBias(Math.random());
-        //var ycen = centerBias(Math.random());
         var xcen = random.gauss(0.5, 0.2);
         var ycen = random.gauss(0.5, 0.2);
-
-        var ysign = 1;
-        if (ycen < 0.5)
-          ysign = -1;
-        var xsign = 1;
-        if (xcen < 0.5)
-          xsign = -1;
 
         var stateModifier = new StateModifier({
           origin: [xcen, ycen]
         });
+        var path = { 'x': 1-xcen,
+                     'y': 1-ycen
+                    };
+        var ysign; var xsign;
+        xsign = xcen < 0.5 ? -1 : 1;
+        ysign = ycen < 0.5 ? -1 : 1;
+        var dest = getDest(path, ysign, xsign);
+
         mainContext.add(stateModifier).add(surface);
         stateModifier.setTransform(
-          Transform.translate((width+(500*Math.random()))*xsign,
-                              (width+(500*Math.random()))*ysign, 0),
+          Transform.translate(dest.x, dest.y, 0),
           {curve: Easing.inExpo, duration: 3000}
         );
       }
     }
+    // returns dest with fields dest.x & dest.y which can be used in Transform.translate(dest.x,dest.y,0)
+    function getDest(path, ysign, xsign) {
+      var x = path.x*width*xsign;
+      var y = path.y*height*ysign;
+      return {
+        x: x,
+        y: y
+      };
+    };
     Timer.setInterval(respawnStars, 10);
 });
